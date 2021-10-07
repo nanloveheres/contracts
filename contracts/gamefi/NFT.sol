d8c29c9a91afff1b3b4bba0cc2e3d32370f06866
Swain-Xie <swain.xie@poloniex.com>
2021-10-07 22:48:19 +0800

    update

--- contracts/gamefi/NFT.sol
+++ contracts/gamefi/NFT.sol
@@ -5,8 +5,6 @@ pragma abicoder v2;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./IManager.sol";
 
